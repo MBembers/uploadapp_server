8dbf474001cd5562b28bf3d9e6e7cb5bad3ec1ca
MBembers <macbed1234@gmail.com>
2022-01-14 10:47:42 +0100

    hm

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
 var express = require("express");
 const path = require("path");
+// var cors = require("cors");
 var app = express();
 var formidable = require("formidable");
 const PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
-app.listen(PORT, () => {
-  console.log("SERVER STARTED ON PORT: " + PORT);
-});
+// app.use(cors());
 
 app.post("/upload", (req, res) => {
-  let form = formidable({});
-  form.multiples = true;
-  form.uploadDir = __dirname + "/static/upload/";
-  form.keepExtensions = true;
-  form.parse(req, function (err, fields, files) {
-    console.log("form parse");
-  });
-  res("amogus");
+  console.log("LEC ADAS LEC");
+  // let form = formidable({});
+  // form.multiples = true;
+  // form.uploadDir = __dirname + "/static/upload/";
+  // form.keepExtensions = true;
+  // form.parse(req, function (err, fields, files) {
+  //   console.log("files:", files, "fields:", fields);
+  //   res.send("amogus");
+  // });
+  res.send("amogus");
+});
+app.get("/upload", (req, res) => {
+  res.send("sussy");
+});
+
+app.listen(PORT, () => {
+  console.log("SERVER STARTED ON PORT: " + PORT);
 });
